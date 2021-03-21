686bca8503ef4c17bbf668bd7c26199103417c83
Chris Esquibel <c.esquibel@herodev.com>
2021-03-21 16:32:13 -0600

    Update php config script

--- scripts/config_php_tail.sh
+++ scripts/config_php_tail.sh
@@ -8,19 +8,21 @@ echo "= OVERRIDE_PATH: [${OVERRIDE_PATH}]"
 echo "============================"
 
 function upsert(){
-  local key=\$1
-  local value=\$2
-  local path=\$3
+  local key=$1
+  local value=$2
+  local path=$3
 
-  grep -Fq "Environment=\"\${key}=" "\${path}"
+  grep -Fq "Environment=\"${key}=" "${path}"
   if [ $? -eq 0 ]; then
-    sed -i "s/^Environment=\"\${key}=.*/Environment=\"\${key}=\${value}\"/" "\${path}"
+	  # update
+    sed -i "s/^Environment=\"${key}=.*/Environment=\"${key}=${value}\"/" "${path}"
   else
+	  # append
     echo "Environment=\"${key}=${value}\"" | tee -a "${path}"
   fi
 }
 
-if [ !-f "${OVERRIDE_PATH}" ]; then
+if [ ! -f "${OVERRIDE_PATH}" ]; then
   sudo sh -c "cat <<EOF >${OVERRIDE_PATH}
 [Service]
 EOF"
